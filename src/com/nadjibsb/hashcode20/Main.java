1ef6e83131b9c7e909b10a96b31d8fe3e8705bf6
NadjibSb <fn_souab@esi.dz>
2020-02-21 16:02:28 +0100

    Docs: add comments

--- src/com/nadjibsb/hashcode20/Main.java
+++ src/com/nadjibsb/hashcode20/Main.java
@@ -26,6 +26,9 @@ public class Main {
             System.out.println("******************\n- Start processing => input file : " + path.getFileName().toString());
             Scanner scanner = new Scanner(path);
 
+
+            /** Read & Initialize ********************************************************************************** **/
+
             // Read the first line (nbr of books, nbr of libraries, nbr of days)
             String[] line1 = scanner.nextLine().split(" ");
             int nbrB = Integer.parseInt(line1[0]);
@@ -34,47 +37,58 @@ public class Main {
 
             // Read the second line (books scores ordered by BookID)
             String[] line2 = scanner.nextLine().split(" ");
-            ArrayList<Book> books = new ArrayList<Book>();
+            ArrayList<Book> books = new ArrayList<>();
             for (int i = 0; i < line2.length; i++) {
                 books.add(new Book(i, Integer.parseInt(line2[i])));
             }
             System.out.println("books initializing finished");
 
             // Read the rest (each library is represented in two lines)
-            ArrayList<Library> libraries = new ArrayList<Library>();
-            int j = 0;
+            ArrayList<Library> libraries = new ArrayList<>();
+            int libraryID = 0;
             while (scanner.hasNextLine()) {
-                //process each line
+
+                //First line contains Library informations (nbr of books, signup duration, books/day)
                 String[] line = scanner.nextLine().split(" ");
-                Library l = new Library(j, Integer.parseInt(line[0]), Integer.parseInt(line[1]), Integer.parseInt(line[2]));
-                //System.out.println(j);
-                ArrayList<Book> bookL = new ArrayList<Book>();
+                Library lib = new Library(libraryID, Integer.parseInt(line[0]), Integer.parseInt(line[1]), Integer.parseInt(line[2]));
+
+                //second line contains books IDs
+                ArrayList<Book> libBooks = new ArrayList<>();
                 String[] bookLine = scanner.nextLine().split(" ");
                 for (int i = 0; i < bookLine.length; i++) {
-                    bookL.add(getBook(books, Integer.parseInt(bookLine[i])));
+                    libBooks.add(getBook(books, Integer.parseInt(bookLine[i])));
                 }
-                l.setBooks(bookL);
-                libraries.add(l);
-                j++;
+                lib.setBooks(libBooks);
+                libraries.add(lib);
+                libraryID++;
             }
             scanner.close();
-
             System.out.println("Libraries initializing finished");
+
+            /** Process ******************************************************************************************** **/
+
             Collections.sort(libraries);
-            System.out.println(libraries.get(0).getScoreTot() + " =>" + libraries.get(1).getScoreTot());
+
+
+            /** Generate output ************************************************************************************ **/
 
             BufferedWriter writer = new BufferedWriter(new FileWriter("output/out_" + path.getFileName().toString()));
-            int lNbr = getLibrariesNbr(libraries, nbrD);
-            writer.write(lNbr + "\n");
-            for (int i = 0; i < lNbr; i++) {
+            int libsToInsert = getLibrariesNbr(libraries, nbrD);
+
+            // the first line contains the total number of libraries to insert
+            writer.write(libsToInsert + "\n");
+
+            //each library is described in 2 lines
+            for (int i = 0; i < libsToInsert; i++) {
+                //first line contains (lib ID, nbr of books to scan )
                 Library lib = libraries.get(i);
                 writer.append(lib.getID() + " " + lib.getBookNbr() + "\n");
 
+                //the second line containes the books to insert
                 String s = "";
                 for (int x = 0; x < lib.getBooks().size(); x++) {
                     s += lib.getBooks().get(x).getId() + " ";
                 }
-
                 writer.append(s + "\n");
             }
 
